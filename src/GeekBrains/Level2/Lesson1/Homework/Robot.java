cc0a8e1eed7cee4d0138cc29a34c24fc57196028
Kirill <arshavin.kirill@mail.ru>
2020-12-23 19:55:22 +0300

    Commit 2.1.8

--- src/GeekBrains/Level2/Lesson1/Homework/Robot.java
+++ src/GeekBrains/Level2/Lesson1/Homework/Robot.java
@@ -1,5 +1,38 @@
 package GeekBrains.Level2.Lesson1.Homework;
 
 public class Robot implements Participants {
+    private static String name;
+    private static int runLimit;
+    private static int jumpLimit;
+    private static boolean flag = true;
 
+    public Robot(String name, int runLimit, int jumpLimit) {
+        this.name = name;
+        this.runLimit = runLimit;
+        this.jumpLimit = jumpLimit;
+    }
+
+    @Override
+    public void run(int trackLength) {
+        if(flag) {
+            if (runLimit >= trackLength) {
+                System.out.println(name + " пробежал дистанцию");
+            } else {
+                System.out.println(name + " не смог пробежать дистанцию и выбывает");
+                flag = false;
+            }
+        }
+    }
+
+    @Override
+    public void jump(int wallHigh) {
+        if(flag) {
+            if (jumpLimit > wallHigh) {
+                System.out.println(name + " перепрыгнул стену");
+            } else {
+                System.out.println(name + " не смог перепрыгнуть стену и выбывает");
+                flag = false;
+            }
+        }
+    }
 }
