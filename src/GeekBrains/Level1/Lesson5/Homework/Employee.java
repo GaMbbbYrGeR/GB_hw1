fbd6f3e586735961577c7be20ad3c21c7212dcfa
Kirill <arshavin.kirill@mail.ru>
2020-12-23 19:48:49 +0300

    Commit 2.1.1

---
+++ src/GeekBrains/Level1/Lesson5/Homework/Employee.java
@@ -0,0 +1,38 @@
+
+package GeekBrains.Level1.Lesson5.Homework;
+
+public class Employee {
+    private String FCS;
+    private String position;
+    private String email;
+    private String telephoneNumber;
+    private String salary;
+    private int age;
+
+    public Employee() {
+
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public Employee(String FCS, String position, String email, String telephoneNumber, String salary, int age) {
+        this.FCS = FCS;
+        this.position = position;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Сотрудник %s.\n" +
+                "Должность: %s\n" +
+                "Электронная почта: %s\n" +
+                "Номер телефона: %s\n" +
+                "Размер зарплаты: %s\n" +
+                "Возраст: %d\n", FCS, position, email, telephoneNumber, salary, age);
+    }
+}
---
+++ src/GeekBrains/Level2/Lesson1/Homework/Task1.java
@@ -0,0 +1,7 @@
+package GeekBrains.Level2.Lesson1.Homework;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
