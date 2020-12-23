95dba047b6e43b7c0835a83b07cc6d9ea885b123
Kirill <arshavin.kirill@mail.ru>
2020-12-23 19:52:51 +0300

    Commit 2.1.7

--- src/GeekBrains/Level2/Lesson1/Homework/Wall.java
+++ src/GeekBrains/Level2/Lesson1/Homework/Wall.java
@@ -1,14 +1,5 @@
 package GeekBrains.Level2.Lesson1.Homework;
 
 public class Wall implements Barriers{
-    private int wallHigh;
 
-    public Wall(int wallHigh) {
-        this.wallHigh = wallHigh;
-    }
-
-    @Override
-    public void moveOverBarrier(Participants P) {
-        P.jump(wallHigh);
-    }
 }
