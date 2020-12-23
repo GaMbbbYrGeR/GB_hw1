a2f839ef1a06473030a5a06eb391e41f98a3bf59
Kirill <arshavin.kirill@mail.ru>
2020-12-23 19:55:22 +0300

    Commit 2.1.8

--- src/GeekBrains/Level2/Lesson1/Homework/Task1.java
+++ src/GeekBrains/Level2/Lesson1/Homework/Task1.java
@@ -2,6 +2,21 @@ package GeekBrains.Level2.Lesson1.Homework;
 
 public class Task1 {
     public static void main(String[] args) {
+        Participants[] participants = {
+                new Cat("Мурзик", 1000, 150),
+                new Robot("R2D2", 50000, 20),
+                new Human("Кирилл", 5000, 100)
+        };
 
+        Barriers[] barriers = {
+                new Track(4000),
+                new Wall(70),
+        };
+
+        for (int i = 0; i < participants.length; i++) {
+            for (int j = 0; j < barriers.length; j++) {
+                barriers[j].moveOverBarrier(participants[i]);
+            }
+        }
     }
 }
--- src/GeekBrains/Level2/Lesson1/Homework/Wall.java
+++ src/GeekBrains/Level2/Lesson1/Homework/Wall.java
@@ -1,5 +1,14 @@
 package GeekBrains.Level2.Lesson1.Homework;
 
 public class Wall implements Barriers{
+    private static int wallHigh;
 
+    public Wall(int wallHigh) {
+        this.wallHigh = wallHigh;
+    }
+
+    @Override
+    public void moveOverBarrier(Participants P) {
+        P.jump(wallHigh);
+    }
 }
