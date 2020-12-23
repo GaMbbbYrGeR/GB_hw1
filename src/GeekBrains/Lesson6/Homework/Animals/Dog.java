f84cff89a7f0ad0a2d7756b107d692b4dd02b787
Kirill <arshavin.kirill@mail.ru>
2020-12-23 19:46:04 +0300

    Revert "Commit 2.1.1"
    This reverts commit d81b5159

---
+++ src/GeekBrains/Lesson6/Homework/Animals/Dog.java
@@ -0,0 +1,33 @@
+package GeekBrains.Lesson6.Homework.Animals;
+
+public class Dog extends Animal{
+
+    static int countOfDogs = 0;
+
+    public int getCountOfDogs() {
+        return countOfDogs;
+    }
+
+    public Dog(String name){
+        super(name);
+        countOfDogs++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if(distance > 500) {
+            System.out.println(name + " пробежал 500 м.");
+        } else {
+            System.out.println(name + " пробежал " + distance + " м.");
+        }
+    }
+
+    @Override
+    public void sweem(int distance) {
+        if(distance > 10) {
+            System.out.println(name + " проплыл 10 метров");
+        } else {
+            System.out.println(name + " проплыл " + distance + " м.");
+        }
+    }
+}
--- src/GeekBrains/Level2/Lesson1/Homework/Task1.java
+++
@@ -1,7 +0,0 @@
-package GeekBrains.Level2.Lesson1.Homework;
-
-public class Task1 {
-    public static void main(String[] args) {
-
-    }
-}
