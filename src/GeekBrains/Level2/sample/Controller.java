5ad9614e8b21ef407a4c3ec90009579b14dfde99
Kirill <arshavin.kirill@mail.ru>
2021-01-02 19:46:57 +0300

    Commit 2.4.1

--- src/GeekBrains/Level2/sample/Controller.java
+++ src/GeekBrains/Level2/sample/Controller.java
@@ -1,21 +1,4 @@
 package GeekBrains.Level2.sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
 public class Controller {
-    @FXML
-    private TextField textField;
-    @FXML
-    private TextArea textArea;
-
-    @FXML
-    public void clickBtnSend() {
-        textArea.appendText(textField.getText()+"\n");
-        textField.clear();
-        textField.requestFocus();
-    }
-
-
 }
--- src/GeekBrains/Level2/sample/Main.java
+++ src/GeekBrains/Level2/sample/Main.java
@@ -4,40 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
-
 public class Main extends Application {
-
-    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
-
-    private TextArea textArea;
-    private TextField textField;
-    private Controller controller;
-
-    private void addKeyHandler(Scene scene) {
-        scene.setOnKeyPressed(ke -> {
-            KeyCode keyCode = ke.getCode();
-            if (keyCode.equals(KeyCode.ENTER)) {
-                controller.clickBtnSend();
-                return;
-            }
-        });
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        Scene scene = new Scene(root, 500, 700);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 500, 700));
         primaryStage.show();
-
-        addKeyHandler(scene);
     }
 
 
