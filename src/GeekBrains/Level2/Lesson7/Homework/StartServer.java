d606cc282a1afae0f6a121eecfb0fbada93b72af
Kirill <arshavin.kirill@mail.ru>
2021-02-27 01:05:41 +0300

    Commit 3.7.2

--- src/GeekBrains/Level2/Lesson7/Homework/StartServer.java
+++ src/GeekBrains/Level2/Lesson7/Homework/StartServer.java
@@ -1,9 +1,8 @@
 package GeekBrains.Level2.Lesson7.Homework;
 
-public class Task7 {
+public class StartServer {
     public static void main(String[] args) {
 
-
-
+        new server.Server();
     }
 }
