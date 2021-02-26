954e56d8dcd1a1ec9e910c6f3bb64636ce8a03f9
Kirill <arshavin.kirill@mail.ru>
2021-02-27 01:05:41 +0300

    Commit 3.7.2

--- src/GeekBrains/Level2/Lesson8/Homework/Server.java
+++ src/GeekBrains/Level2/Lesson8/Homework/Server.java
@@ -1,4 +1,4 @@
-package GeekBrains.Level2.Lesson8.Homework;
+package GeekBrains.Level2.Lesson7.Homework;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -38,61 +38,22 @@ public class Server {
         }
     }
 
-    public void broadcastMsg(ClientHandler clientHandler, String msg) {
+    public void broadcastMsg(ClientHandler clientHandler, String msg){
         String message = String.format("[ %s ]: %s", clientHandler.getNickname(), msg);
         for (ClientHandler c : clients) {
             c.sendMsg(message);
         }
     }
 
-    public void privateMsg(ClientHandler sender, String receiver, String msg) {
-        String message = String.format("[ %s ] to [ %s ]: %s", sender.getNickname(), receiver, msg);
-        for (ClientHandler c : clients) {
-            if (c.getNickname().equals(receiver)) {
-                c.sendMsg(message);
-                if (!c.equals(sender)) {
-                    sender.sendMsg(message);
-                }
-                return;
-            }
-        }
-        sender.sendMsg(String.format("User %s not found", receiver));
-    }
-
-    void subscribe(ClientHandler clientHandler) {
+    void subscribe(ClientHandler clientHandler){
         clients.add(clientHandler);
-        broadcastClientList();
     }
 
-    void unsubscribe(ClientHandler clientHandler) {
+    void unsubscribe(ClientHandler clientHandler){
         clients.remove(clientHandler);
-        broadcastClientList();
     }
 
     public AuthService getAuthService() {
         return authService;
-    }
-
-    public boolean isLoginAuthenticated(String login) {
-        for (ClientHandler c : clients) {
-            if (c.getLogin().equals(login)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void broadcastClientList() {
-        StringBuilder sb = new StringBuilder(Command.CLIENT_LIST);
-
-        for (ClientHandler c : clients) {
-            sb.append(" ").append(c.getNickname());
-        }
-
-        String msg = sb.toString();
-
-        for (ClientHandler c : clients) {
-            c.sendMsg(msg);
-        }
     }
 }
