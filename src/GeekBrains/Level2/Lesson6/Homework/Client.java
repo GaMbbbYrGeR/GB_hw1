3f703110898defdb236e76ced4f537c4c5cbe1fd
Kirill <arshavin.kirill@mail.ru>
2021-03-03 22:56:28 +0300

    Commit 3.8.3

--- src/GeekBrains/Level2/Lesson6/Homework/Client.java
+++
@@ -1,5 +0,0 @@
-package GeekBrains.Level2.Lesson6.Homework;
-
-public class Client {
-
-}
--- src/GeekBrains/Level3/Lesson1/Homework/Task1.java
+++ src/GeekBrains/Level3/Lesson1/Homework/Task1.java
@@ -1,7 +1,12 @@
 package GeekBrains.Level3.Lesson1.Homework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
 
     }
+
 }
